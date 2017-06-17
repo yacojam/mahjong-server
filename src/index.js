b3eaecff141c5e29f69b7d4e79b33b8b5d3502b0
Maxwin-z <zwd2005@gmail.com>
2017-06-17 10:24:59 +0800

    add io socket

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import './index.css'
+import io from 'socket.io-client'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const socket = new io(':8080')
+socket.on('data', data => {
+  console.log(data)
+})
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
