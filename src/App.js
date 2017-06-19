9c937c2d93158f41e42ec869f58aafeb08c3ae6a
zwd2005 <zwd2005@gmail.com>
2017-06-19 17:05:34 +0800

    creat room simple

--- src/App.js
+++ src/App.js
@@ -3,30 +3,47 @@ import './App.css'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Box, { VBox } from 'react-layout-components'
-import AddUser from './AddUser'
 import AddRoom from './AddRoom'
 
+import RaisedButton from 'material-ui/RaisedButton'
 injectTapEventPlugin()
 class App extends Component {
   constructor(context) {
     super(context)
-    this.state = { roomid: 0 }
+    this.state = { rooms: [] }
   }
   onRoomCreate = roomid => {
     this.setState({ roomid })
   }
+  componentDidMount = () => {
+    fetch('/rooms').then(rsp => rsp.json()).then(ret => {
+      console.log(ret)
+      if (ret.code !== 0) {
+        alert(ret.message)
+        return
+      }
+      this.setState({
+        rooms: ret.data || []
+      })
+    })
+  }
   render() {
+    const style = {
+      marginRight: 10
+    }
     return (
       <MuiThemeProvider>
         <VBox style={{ padding: 20 }}>
-          <Box height={500}>
-            {this.state.roomid
-              ? 'game: ' + this.state.roomid
-              : 'NOT ROOM, CREATE ONE PLZ'}
-          </Box>
+          {this.state.rooms.map(room => (
+            <RaisedButton
+              style={style}
+              key={room}
+              label={'join: ' + room}
+              primary={true}
+            />
+          ))}
           <hr />
           <AddRoom onRoomCreate={this.onRoomCreate} />
-          <AddUser />
         </VBox>
       </MuiThemeProvider>
     )
