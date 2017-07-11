6161e30cfc9bc4fb8ab20f3fa2954736f51634f1
Maxwin-z <zwd2005@gmail.com>
2017-07-11 22:06:46 +0800

    fix server error

--- server/routers/roomService.js
+++ server/routers/roomService.js
@@ -5,45 +5,42 @@ const redis = require('../redis/redisHelper')
 const ErrorType = require('./ServerError')
 const Router = require('koa-router')
 const router = new Router()
-const HxRulesUtils = require('./roomRuleUtil/HxRulesUtils')
+const HxRulesUtils = require('../roomManager/roomRuleUtil/HxRulesUtils')
 
 router.get('/get_rules', async (ctx, next) => {
-    var userid = ctx.query.userid
-    var deviceid = ctx.query.deviceid
-    var token = ctx.query.token
-    var isValid = true //await redis.isAccountValid(userid, deviceid, token);
-    if (isValid) {
-        ctx.body = HxRulesUtils.rules
-    } else {
-        ctx.error = ErrorType.AccountValidError
-    }
+  var userid = ctx.query.userid
+  var deviceid = ctx.query.deviceid
+  var token = ctx.query.token
+  var isValid = true //await redis.isAccountValid(userid, deviceid, token);
+  if (isValid) {
+    ctx.body = HxRulesUtils.rules
+  } else {
+    ctx.error = ErrorType.AccountValidError
+  }
 })
 
 //创建房间
 router.get('/create_private_room', async (ctx, next) => {
-    var userid = ctx.query.userid
-    var deviceid = ctx.query.deviceid
-    var token = ctx.query.token
-    var roomInfo = ctx.query.roomInfo
-    var isValid = await redis.isAccountValid(userid, deviceid, token)
-    if (isValid) {
-        var cardnum_of_user = await UserDao.sycn_get_card_of_account(userid)
-        if (cardnum_of_user == 0) {
-            ctx.error = ErrorType.CardNotEnoughError
-        } else {
-            cardrulenum = HxRulesUtils.getCardOfRule(roomInfo[0])
-            if (cardrulenum > cardnum_of_user) {
-                ctx.error = ErrorType.CardNotEnoughError
-            } else {
-                //开始创建房间，生成6为展示ID并返回，数据库存储该房间
-            }
-        }
+  var userid = ctx.query.userid
+  var deviceid = ctx.query.deviceid
+  var token = ctx.query.token
+  var roomInfo = ctx.query.roomInfo
+  var isValid = await redis.isAccountValid(userid, deviceid, token)
+  if (isValid) {
+    var cardnum_of_user = await UserDao.sycn_get_card_of_account(userid)
+    if (cardnum_of_user == 0) {
+      ctx.error = ErrorType.CardNotEnoughError
     } else {
-        ctx.error = ErrorType.AccountValidError
+      cardrulenum = HxRulesUtils.getCardOfRule(roomInfo[0])
+      if (cardrulenum > cardnum_of_user) {
+        ctx.error = ErrorType.CardNotEnoughError
+      } else {
+        //开始创建房间，生成6为展示ID并返回，数据库存储该房间
+      }
     }
-})
-
-//进入房间
+  } else {
+    ctx.error = ErrorType.AccountValidError
+  }
+}) //进入房间
 router.get('/join_private_room', async (ctx, next) => {})
-
 module.exports = router
