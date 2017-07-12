80dda888af8eb3e7365e4615af0da9940b919705
yinchen <Chenyin@fanruan.com>
2017-07-12 10:51:10 +0800

    fix error

--- server/roomManager/roomRuleUtil/hxdzRule.js
+++ server/roomManager/roomRuleUtil/hxdzRule.js
@@ -3,25 +3,25 @@ function generateEmptyRule() {
 	rule.numOfJu = 0
 	rule.dfOfJu = 0
 	//是不是随风三大砍
-	this.isFengForThreeKan = false
+	rule.isFengForThreeKan = false
 	//逢双拿不拿，除双杠，双连号，但不包括三杠
-	this.isNaForDouble = false
+	rule.isNaForDouble = false
 	//三杠拿不拿
-	this.isNaFor3Gang = false
+	rule.isNaFor3Gang = false
 	//清一色拿不拿
-	this.isNaForQYS = false
+	rule.isNaForQYS = false
 	//31点加10点拿不拿
-	this.isNaFor31 = false
+	rule.isNaFor31 = false
 	//三大砍带头拿不拿
-	this.isNaForThreeKanAndTou = false
+	rule.isNaForThreeKanAndTou = false
 	//中发白拿不拿
-	this.isNaForZFB = false
+	rule.isNaForZFB = false
 	//暗老小头拿不拿
-	this.isNaForAnLXT = false
+	rule.isNaForAnLXT = false
 	//点炮50点，自摸100点
-	this.isNaFor50Point = false
+	rule.isNaFor50Point = false
 	//大跳车自摸拿不拿
-	this.isNaForDTCZM = false
+	rule.isNaForDTCZM = false
 }
 
 module.exports.generateEmptyRule = generateEmptyRule
