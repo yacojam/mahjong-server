b4a98df5775fdea3b0084cda0945456269598346
zwd2005 <zwd2005@gmail.com>
2017-07-13 21:29:48 +0800

    add gang

---
+++ server/algorithm/room/actionGang.js
@@ -0,0 +1,11 @@
+const HXMJManager = require('../HxmjRules/HxmjManager')
+const Action = require('../HxmjRules/hxaction')
+const utils = require('./utils')
+
+async function gang(action) {
+  const { user, room, pai } = action
+
+  return room
+}
+
+module.exports = gang
