04b5fe7fbee7822e10c77a165de3d8381d27c19d
Maxwin-z <zwd2005@gmail.com>
2017-07-16 00:30:15 +0800

    add winner

--- src/room/Room.js
+++ src/room/Room.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
 import User from './User'
 
+class Winner extends Component {
+  onClick = () => {
+    //
+  }
+  render() {
+    const { name, score } = this.props.user
+    return (
+      <div>
+        <h1>
+          {name} win {score.join('/')}
+          <button onClick={this.onClick}>new game</button>
+        </h1>
+      </div>
+    )
+  }
+}
+
 class Room extends Component {
   render() {
     const { room } = this.props
@@ -8,6 +25,11 @@ class Room extends Component {
     return (
       <div>
         <div>id: {room.id}</div>
+        {room.state === 'DONE'
+          ? room.users.map(user => {
+              return user.score ? <Winner key={user.uid} user={user} /> : null
+            })
+          : null}
         <div>
           {room.users.map(user => <User user={user} key={user.uid} />)}
         </div>
